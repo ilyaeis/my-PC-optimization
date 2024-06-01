10fd0dd3db9b051a4b196c011be8fea11ec657c4
ilya eis <ilya.safronos@gmail.com>
2024-06-02 00:26:03 +0300

    First optimization

--- sort_photo_by_date_in_folder.py
+++ sort_photo_by_date_in_folder.py
@@ -4,18 +4,6 @@ from datetime import datetime
 import shutil
 
 def copy_and_rename_os_safe(src_path, dest_path, new_name):
-    """
-    Copies a file from source to destination with a new name, handling potential permission issues gracefully.
-
-    Args:
-        src_path (str): Path to the source file.
-        dest_path (str): Path to the destination directory.
-        new_name (str): The new name for the copied file.
-
-    Returns:
-        bool: True if the copy operation was successful, False otherwise.
-    """
-
     try:
         # Attempt to copy the file using os.copyfile
         shutil.copy(src_path, f"{dest_path}\\{new_name}")
@@ -36,14 +24,6 @@ def get_exif_key(img, key):
     return exif_data[key]  
   
 def filter_images(source_dir, dest_dir):
-    """
-    Filters and copies images from source to destination directory based on EXIF data.
-
-    Args:
-        source_dir (str): Path to the source directory.
-        dest_dir (str): Path to the destination directory.
-    """
-
     for filename in os.listdir(source_dir):
         if not filename.lower().endswith(('.jpg', '.png')):
             continue
@@ -58,34 +38,24 @@ def filter_images(source_dir, dest_dir):
             time = get_exif_key(img, key_to_check)
 
             if time:
-                new_name = f"{convert_to_miseconds(time)}_{filename}"
+                new_name = f"{convert_to_seconds(time)}_{filename}"
                 copy_success = copy_and_rename_os_safe(src_path, dest_dir, new_name)
                 if not copy_success:
                     print(f"Failed to copy {src_path} to {dest_dir}/{new_name}")
         except (OSError, IOError) as e:
             print(f"Error processing {filename}: {e}")
 
-def convert_to_miseconds(date_time_str):
-    """
-    Converts a date/time string (expected format) to milliseconds since epoch.
-
-    Args:
-        date_time_str (str): The date/time string to convert.
-
-    Returns:
-        int: The time in milliseconds since epoch, or None if parsing fails.
-    """
-
+def convert_to_seconds(date_time_str):
     try:
         datetime_obj = datetime.strptime(date_time_str, "%Y:%m:%d %H:%M:%S")
         epoch = datetime.utcfromtimestamp(0)
-        return int((datetime_obj - epoch).total_seconds() * 1000)
+        return int((datetime_obj - epoch).total_seconds())
     except TypeError:
         print(f"Invalid date/time format: {date_time_str}")
         return None
 
 if __name__ == "__main__":
-    path = "C:\\Users\\Lenovo\\Pictures\\Gallery\\Camera"
-    tpath = "C:\\Users\\Lenovo\\Pictures\\Gallery\\test"
+    path = "...\\Pictures\\Gallery\\Camera"
+    tpath = "...\\Pictures\\Gallery\\CompletePhotocolection"
 
     filter_images(path, tpath)
